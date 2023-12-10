fdf5a4242c9480c927e760cfc53bdebf07dd184a
Max Romanovskii <151863055+maxromanovskii@users.noreply.github.com>
2023-12-10 19:22:58 +0300

    Update script.sh
    commot

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# function to display help message
 display_help() {
   echo "Usage: $0 [options] directory"
   echo "Options:"
@@ -9,7 +8,6 @@ display_help() {
   echo "  -h, --help                 Display this help message"
 }
 
-# parse command-line arguments
 while [[ $# -gt 0 ]]
 do
   key="$1"
@@ -17,13 +15,13 @@ do
   case $key in
     -c|--compress)
       compress="$2"
-      shift # past argument
-      shift # past value
+      shift
+      shift
       ;;
     -o|--output)
       output="$2"
-      shift # past argument
-      shift # past value
+      shift
+      shift
       ;;
     -d|--directory)
       directory="$2"
@@ -35,7 +33,7 @@ do
       exit 0
       ;;
     *)
-      # unknown option
+    
       echo "Unknown option"
       display_help
       exit 0
@@ -43,39 +41,29 @@ do
   esac
 done
 
-# check if directory exists
 if [[ ! -d $directory ]]; then
   echo "Error: Directory '$directory' does not exist"
   exit 1
 fi
 
-# check if compression algorithm is provided
 if [[ -z $compress ]]; then
   echo "Error: Compression algorithm is required"
   display_help
   exit 1
 fi
 
-# check if output file name is provided
 if [[ -z $output ]]; then
   echo "Error: Output file name is required"
   display_help
   exit 1
 fi
 
-# create backup archive
 tar cf - "$directory" | $compress > "$output"
 
-# encrypt backup archive
 openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 1000000 -salt -in "$output" -out "$output.enc"
 
-# redirect all output except errors to /dev/null
 exec 1>/dev/null
 
-# write errors to error.log file
 exec 2>error.log
-
-echo "Backup completed successfully"
-
 
 
